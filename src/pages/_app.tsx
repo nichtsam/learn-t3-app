f7e32357b527af89b1e45a531920a453f857813e
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2023-03-26 18:14:32 +0800

    add highlight monitoring frontend

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,6 @@
-import { ClerkProvider } from "@clerk/nextjs";
+import { ErrorBoundary } from "@highlight-run/react";
+import { H } from "highlight.run";
+import { ClerkProvider, useUser } from "@clerk/nextjs";
 import { type AppType } from "next/app";
 import relativeTime from "dayjs/plugin/relativeTime";
 import dayjs from "dayjs";
@@ -9,12 +11,37 @@ import "~/styles/globals.css";
 
 dayjs.extend(relativeTime);
 
+H.init("kevnn3g3", {
+  tracingOrigins: true,
+  networkRecording: {
+    enabled: true,
+    recordHeadersAndBody: true,
+    urlBlocklist: [
+      // insert urls you don't want to record here
+    ],
+  },
+});
+
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
-    </ClerkProvider>
+    <ErrorBoundary>
+      <ClerkProvider {...pageProps}>
+        <IdentifyUser />
+        <Component {...pageProps} />
+      </ClerkProvider>
+    </ErrorBoundary>
   );
+};
+
+const IdentifyUser = () => {
+  const { user, isSignedIn } = useUser();
+  if (isSignedIn) {
+    H.identify(user.primaryEmailAddress?.emailAddress ?? user.id, {
+      id: user.id,
+    });
+  }
+
+  return null;
 };
 
 export default api.withTRPC(MyApp);
