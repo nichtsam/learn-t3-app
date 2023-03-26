f06982d785f53a10e3a3b5a7b69f87a79c7ff34e
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2023-03-27 00:33:17 +0800

    better zod error

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -50,7 +50,7 @@ export const postRouter = createTRPCRouter({
   create: privateProcedure
     .input(
       z.object({
-        content: z.string().emoji().min(1).max(255),
+        content: z.string().emoji("Only emojis are allowed!").min(1).max(255),
       })
     )
     .mutation(async ({ ctx, input: { content } }) => {
