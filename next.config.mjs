6e9dcb2e0777409b16bd635adcd21d0ef24661f6
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2023-03-26 18:24:05 +0800

    add highlight monitoring backend

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,5 @@
+import { withHighlightConfig } from "@highlight-run/next";
+
 /**
  * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation.
  * This is especially useful for Docker builds.
@@ -5,21 +7,27 @@
 !process.env.SKIP_ENV_VALIDATION && (await import("./src/env.mjs"));
 
 /** @type {import("next").NextConfig} */
-const config = {
-  reactStrictMode: true,
-  images: {
-    domains: ["images.clerk.dev"],
-  },
+const nextConfig = {
+    reactStrictMode: true,
+    images: {
+        domains: ["images.clerk.dev"],
+    },
 
-  /**
-   * If you have the "experimental: { appDir: true }" setting enabled, then you
-   * must comment the below `i18n` config out.
-   *
-   * @see https://github.com/vercel/next.js/issues/41980
-   */
-  i18n: {
-    locales: ["en"],
-    defaultLocale: "en",
-  },
+    /**
+     * If you have the "experimental: { appDir: true }" setting enabled, then you
+     * must comment the below `i18n` config out.
+     *
+     * @see https://github.com/vercel/next.js/issues/41980
+     */
+    i18n: {
+        locales: ["en"],
+        defaultLocale: "en",
+    },
 };
+
+const config = () => {
+    const plugins = [withHighlightConfig];
+    return plugins.reduce((acc, next) => next(acc), nextConfig);
+};
+
 export default config;
