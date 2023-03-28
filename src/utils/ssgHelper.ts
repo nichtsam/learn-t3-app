e9819015cddfa4649bbd2997157940d3b3bdbb4b
Samuel Jensen <44519206+nichtsam@users.noreply.github.com>
2023-03-28 21:04:48 +0800

    ssgHelper superjson

--- src/utils/ssgHelper.ts
+++ src/utils/ssgHelper.ts
@@ -1,9 +1,11 @@
 import { createProxySSGHelpers as createInnerProxySSGHelpers } from "@trpc/react-query/ssg";
 import { appRouter } from "~/server/api/root";
 import { createInnerTRPCContext } from "~/server/api/trpc";
+import superjson from "superjson";
 
 export const createProxySSGHelpers = () =>
   createInnerProxySSGHelpers({
     router: appRouter,
     ctx: { ...createInnerTRPCContext({}), currentUserId: null },
+    transformer: superjson, // optional - adds superjson serialization
   });
